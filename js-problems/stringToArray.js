ecebaacfd8fb9d6abbc00218b7e410ee64ff5454
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2022-12-04 17:04:07 -0500

    Add files via upload
    takes a string and returns an array containing each word from that string

---
+++ js-problems/stringToArray.js
@@ -0,0 +1,8 @@
+//create a function that takes a string and returns an array of 
+//words (a sentence gets broken down into an array of every word)
+
+function stringArr(string){
+    return string.split(' ')
+} 
+
+console.log(stringArr("hello my name is Carter."))
