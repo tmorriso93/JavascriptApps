3b66f499fcd226917cd26da7709712ffeb4ea1bd
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2023-02-08 00:03:40 -0500

    Add files via upload
    Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
    
    Write a function which takes a list of strings and returns each line prepended by the correct number.
    
    The numbering starts at 1. The format is n: string. Notice the colon and space in between.
     
    example: if you enter ["a", "b", "c"]    the function should return this --> ["1: a", "2: b", "3: c"]

---
+++ js-problems/test123.js
@@ -0,0 +1,10 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+const number = (array) =>
+    array.map((element,index) => `${index + 1}: ${element}`)
+
+    console.log(number(["a", "b", "c"]))
