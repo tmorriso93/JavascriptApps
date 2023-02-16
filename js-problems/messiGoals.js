037771beb74fab3d265527e51929cd8c63fd49b1
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2023-02-15 22:46:44 -0500

    Add files via upload
    Messi is a soccer player, write a function that takes
    the amount of goals he's scored in the last 3 games

---
+++ js-problems/messiGoals.js
@@ -0,0 +1,8 @@
+//Messi is a soccer player, write a function that takes
+// the amount of goals he's scored in the last 3 games
+
+function goals (monday, tuesday, wednesday) {
+    return total = monday + tuesday + wednesday;
+    }
+
+    console.log(goals(2, 5, 3));
