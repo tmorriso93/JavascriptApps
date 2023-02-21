44d9665cac089bec089080850124ec11ebde82e2
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2023-02-20 22:40:01 -0500

    Add files via upload
    create a function that will tell you if a number is
    odd or even

---
+++ js-problems/evenOrOdd.js
@@ -0,0 +1,10 @@
+// create a function that will tell you if a number is
+// odd or even
+
+function evenOrOdd(number) {
+    if (number % 2===0){
+      return number + ": Even"
+    } else {
+      return number + ": Odd"
+    }
+  }
