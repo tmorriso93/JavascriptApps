4316cd56fbb70ab471e9bb929107630f562e08cb
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2023-02-21 19:34:40 -0500

    Add files via upload
    create a function that will
    find the last item in an array

---
+++ js-problems/lastInArr.js
@@ -0,0 +1,9 @@
+// create a function that will
+// find the last item in an array
+
+function findLastNum(arr){
+    let lastNum = arr[arr.length - 1];
+    return console.log(lastNum)
+}
+
+findLastNum([4, 9, 10])
