5cf58719cd9586cd5f5b5f632956d115c1929a9f
tmorriso93 <36183040+tmorriso93@users.noreply.github.com>
2023-03-02 14:32:04 -0500

    Add files via upload
    convert numbers to strings

---
+++ js-problems/num2String.js
@@ -0,0 +1,11 @@
+// turn a number into a string
+
+function numToString(num){
+    return num.toString()
+}
+
+console.log(numToString(12345))
+
+
+//ES6
+// const numberToString = num => num.toString();
